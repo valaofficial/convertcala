273509eef02bd122810063b1fbfb45dd45847add
Vala <mhyelavala@gmail.com>
2021-08-02 13:39:03 +0100

    Updates

---
+++ precache-manifest.22803be482c6282acaa8dd7ffc25c4fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b422e1de07c446af86359a6d668fd517",
+    "url": "/convertcala/index.html"
+  },
+  {
+    "revision": "e59069f6d3d2922dfcf6",
+    "url": "/convertcala/static/css/main.0be8d511.chunk.css"
+  },
+  {
+    "revision": "55882e474c6f9ef47618",
+    "url": "/convertcala/static/js/2.09e1d1cb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/convertcala/static/js/2.09e1d1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e59069f6d3d2922dfcf6",
+    "url": "/convertcala/static/js/main.e7c2394c.chunk.js"
+  },
+  {
+    "revision": "65d0c30c309a3495130b",
+    "url": "/convertcala/static/js/runtime-main.d4fb8b81.js"
+  },
+  {
+    "revision": "d6eca7fa9137d29619313c94d0e1a4d0",
+    "url": "/convertcala/static/media/loading.d6eca7fa.png"
+  }
+]);
